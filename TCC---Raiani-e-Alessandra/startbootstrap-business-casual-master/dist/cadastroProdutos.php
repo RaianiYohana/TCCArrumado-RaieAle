d4952afef88c5e3c6dcc8172a13ee4206bb84628
RaianiYohana <raianirescarolli@gmail.com>
2024-09-03 09:25:03 -0700

    .

--- TCC---Raiani-e-Alessandra/startbootstrap-business-casual-master/dist/cadastroProdutos.php
+++ TCC---Raiani-e-Alessandra/startbootstrap-business-casual-master/dist/cadastroProdutos.php
@@ -1,22 +1,25 @@
 <?php
-include 'db_connection.php'
-if ($_SERVER["REQUEST_METHOD"]== "POST"){
+include 'db_connection.php';
+
+if ($_SERVER["REQUEST_METHOD"] == "POST") {
     $tipo = $_POST ['tipo'];
     $descricao = $_POST ['descricao'];
     $aro = $_POST ['aro'];
     $preco = $_POST ['preco'];
     $marca = $_POST ['marca'];
     
-    $sql = "INSERT INTO produto (tipo, descricao, aro, preco, marca) VALUES (?,?,?,?,?) ";
-}
+    $sql = "INSERT INTO produto (tipo, descricao, aro, preco, marca) VALUES (?,?,?,?,?)";
+
 if ($stmt = $conn->prepare($sql)){
-    $stmt->bind_param("ssssss", $tipo, $descricao, $aro, $preco, $marca);
+    $stmt->bind_param("ssss", $tipo, $descricao, $aro, $preco, $marca);
     if($stmt->execute()){
         echo "Novo produto cadastrado com sucesso.";
     } else {
-echo "Erro:" . $stmt->error;
+echo "Erro: " . $stmt->error;
     }
     $stmt->close();
 }
-$sconn->close();
+}
+
+$conn->close();
 ?>
