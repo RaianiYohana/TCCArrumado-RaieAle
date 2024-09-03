ee2a48a7031f561ef7511c04e56d235dadb295fb
iugi <160235673+AlessandraPenz@users.noreply.github.com>
2024-09-03 09:05:32 -0300

    Cadastro de produtos no php
    continuação do tcc

---
+++ TCC---Raiani-e-Alessandra/startbootstrap-business-casual-master/dist/cadastroProdutos.php
@@ -0,0 +1,22 @@
+<?php
+include 'db_connection.php'
+if ($_SERVER["REQUEST_METHOD"]== "POST"){
+    $tipo = $_POST ['tipo'];
+    $descricao = $_POST ['descricao'];
+    $aro = $_POST ['aro'];
+    $preco = $_POST ['preco'];
+    $marca = $_POST ['marca'];
+    
+    $sql = "INSERT INTO produto (tipo, descricao, aro, preco, marca) VALUES (?,?,?,?,?) ";
+}
+if ($stmt = $conn->prepare($sql)){
+    $stmt->bind_param("ssssss", $tipo, $descricao, $aro, $preco, $marca);
+    if($stmt->execute()){
+        echo "Novo produto cadastrado com sucesso.";
+    } else {
+echo "Erro:" . $stmt->error;
+    }
+    $stmt->close();
+}
+$sconn->close();
+?>
